f7d24cf33315998108528def3c89eb681ab6d7b4
stefann-01 <st.nikolic01@gmail.com>
2025-05-31 05:27:45 +0200

    add api funcs

---
+++ frontend/back/api.ts
@@ -0,0 +1,71 @@
+import axios from 'axios';
+
+interface TweetResponse {
+  tweet: {
+    full_text: string;
+    id_str: string;
+  };
+  user: {
+    legacy: {
+      screen_name: string;
+    };
+  };
+}
+
+interface UserResponse {
+  users: Array<{
+    legacy: {
+      profile_image_url_https: string;
+    };
+  }>;
+}
+
+export async function createPost(tweetUrl: string) {
+  try {
+    const tweetId = tweetUrl.split('/').pop();
+    const response = await axios.request<TweetResponse>({
+      method: 'GET',
+      url: 'https://twitter241.p.rapidapi.com/tweet',
+      params: { pid: tweetId },
+      headers: {
+        'x-rapidapi-key': 'c61beeff96msh67d12151c563241p13be66jsn2d6ef66ed4c3',
+        'x-rapidapi-host': 'twitter241.p.rapidapi.com'
+      }
+    });
+    
+    const { tweet, user } = response.data;
+    return {
+      screen_name: user.legacy.screen_name,
+      full_text: tweet.full_text,
+      id_str: tweet.id_str
+    };
+  } catch (error) {
+    console.error('Error fetching tweet:', error);
+    throw error;
+  }
+}
+
+// Example usage:
+// createPost('https://twitter.com/username/status/1234567890');
+
+
+export async function getUserProfileImg(userId: string) {
+  try {
+    const response = await axios.request<UserResponse>({
+      method: 'GET',
+      url: 'https://twitter241.p.rapidapi.com/get-users',
+      params: {
+        users: userId
+      },
+      headers: {
+        'x-rapidapi-key': 'c61beeff96msh67d12151c563241p13be66jsn2d6ef66ed4c3',
+        'x-rapidapi-host': 'twitter241.p.rapidapi.com'
+      }
+    });
+    
+    return response.data.users[0].legacy.profile_image_url_https;
+  } catch (error) {
+    console.error('Error fetching user profile:', error);
+    throw error;
+  }
+}
